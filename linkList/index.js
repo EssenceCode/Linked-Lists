951b66740ffd4be423b9d43e9ada0dc348e3488e
EssenceCode <123456789+odin@users.noreply.github.com>
2023-11-14 15:01:13 +0800

    created a linklist factory that can add a new node to the end of the list

--- linkList/index.js
+++ linkList/index.js
@@ -5,3 +5,25 @@ function Node(value = null, nextNode = null) {
     };
 };
 
+function LinkedList(val) {
+    const head = Node(val);
+    const getList = () => head;
+
+    const append = (value, obj = head) => {
+        const list = obj;
+        if(list.nextNode === null) {
+            list.nextNode = Node(value, list.nextNode);
+
+            return list;
+        };
+
+        return append(value, list.nextNode);
+    };
+  
+
+    return {
+        append,
+    }
+};
+
+const list = LinkedList();
