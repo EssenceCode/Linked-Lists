f5dcca1b1cbda0afab8429de654c03a7e9a38b42
EssenceCode <123456789+odin@users.noreply.github.com>
2023-11-16 10:10:54 +0800

    created a pop() method that will remove the last element from a list

--- linkList/index.js
+++ linkList/index.js
@@ -1,89 +1,94 @@
-// Node class / factory, containing a 'value' property, 
-// and a link to the 'nextNode' set both as null by default.
 function Node(value = null, nextNode = null) {
     return {
         value,
         nextNode
     };
 };
-// LinkedList class / factory, which will represent the full list.
+
 function LinkedList(val) {
     const root = Node(val);
     const getList = () => root;
 
-    // append(value) adds a new node containing value to the end of the list
     const append = (value, obj = root) => {
-        // base case to stop the recursion
-        // if list.nextNode is equals to null
-        // list.nextNode = Node(value, list.nextNode)
-
-        // recursively call the function till the condition is met
-        // append(value, list.nextNode)
         const list = obj;
         if(list.nextNode === null) {
             list.nextNode = Node(value, list.nextNode);
 
             return list;
         };
-        // use recursion till it satisfy the base case
+      
         return append(value, list.nextNode);
     };
-    // prepend(value) adds a new node containing value to the start of the list
+  
     const prepend = (value, obj = root) => {
-        // base case to stop the recursion
-        // if list.value is equals to null or not null
-        // list.value = value
-
-        // recursively call the function till the condition is met
-        // prepend(value, list.nextNode)
         const list = obj;
         if(list.value === null || list.value !== null) {
             list.value = value;
 
             return list;
         };
-        // use recursion till it satisfy the base case
+       
         return prepend(value, list.nextNode);
     };
-    // size returns the total number of nodes in the list
+    
     const size = (list = root, counter = 1) => {
-        // base case to stop the recursion
-        // initialized a variable counter
-        // base case to stop the recursion
-        // if the list.nextNode is equals to null return counter
         if(list.nextNode === null) {
-            console.log(list.value)
+
             return counter;
         };
-        // use recursion to increased the counter variable till,
-        // it satisfy the base case
+      
         return size(list.nextNode, counter + 1);
     };
-    // head returns the first node in the list
-    // just return the list?
+    
     const head = (list = root) => list;
-    // tail returns the last node in the list
+   
     const tail = (list = root) => {
-        // base case to stop the recursion
-        // if list.nextNode === null
-        // return list
         if(list.nextNode === null) {
            
             return list;
         };
-        // use recursion till it satisfy the base case
+       
         return tail(list.nextNode);
     };
-    // at(index) returns the node at the given index
+  
+    const at = (index,  counter = 1, list = root) => {
+       
+       if(index === counter) {
+
+        return list;
+       };
+     
+       if(index !== counter && list.nextNode === null) {
+
+        return "index does not exist";
+       };
+      
+       return at(index, counter + 1, list.nextNode);
+    };
    
+    const pop = (list = root, previous = list) => {
+        let current = null;
+
+        if(list.nextNode === null) {
+            current = previous;
+            current.nextNode = null;
+            
+            return true;
+        };
+
+        current = list;
+
+        return pop(list.nextNode, current)
+    }; 
     return {
         append,
         prepend,
         size,
         head,
         tail,
+        pop,
         getList,
-        root,
+        
     }
 };
 
@@ -98,4 +103,8 @@ list.append(7)
 list.append(8)
 list.append(9)
 list.append(10)
+list.append(11)
+list.append(12)
+
+
 
